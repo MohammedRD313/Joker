99193efcb6995652632041e1d817902f543239b4
MohammedRD313 <hamodeygame45@gmail.com>
2024-07-09 15:06:30 +0300

    Update فحص.py

--- JoKeRUB/plugins/فحص.py
+++ JoKeRUB/plugins/فحص.py
@@ -44,7 +44,7 @@ async def amireallyalive(event):
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
-    EMOJI = gvarstatus("ALIVE_EMOJI") or "⿻┊‌‎"
+    EMOJI = gvarstatus("ALIVE_EMOJI") or "✎┊‌"
     ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**╔==========================╗ **"
     HuRe_IMG = gvarstatus("ALIVE_PIC") or Config.A_PIC
     l313l_caption = gvarstatus("ALIVE_TEMPLATE") or temp
