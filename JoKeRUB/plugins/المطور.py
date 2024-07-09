e0b1fc5aa16b6a7d120f77a159ffe60474f2e181
MohammedRD313 <hamodeygame45@gmail.com>
2024-07-10 00:31:39 +0300

    Update المطور.py

--- JoKeRUB/plugins/المطور.py
+++ JoKeRUB/plugins/المطور.py
@@ -49,7 +49,7 @@ async def amireallyalive(event):
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
     CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT")
-    CAT_IMG = "https://telegra.ph/file/e35ea3f43faab1e0d9d91.jpg"
+    CAT_IMG = "https://telegra.ph/file/fa79f4b90e30e1398f08d.jpg"
     if CAT_IMG:
         CAT = [x for x in CAT_IMG.split()]
         A_IMG = list(CAT)
