95dda090e192f2ea526bf8a02f282f681d1b8215
MohammedRD313 <hamodeygame45@gmail.com>
2024-07-10 00:00:19 +0300

    Update ياعلي.py

--- JoKeRUB/plugins/ياعلي.py
+++ JoKeRUB/plugins/ياعلي.py
@@ -13,7 +13,7 @@ from ..core.managers import edit_delete, edit_or_reply
 #اخ اخ اخ اخ اخ اخ اخممممممط ياطويل العمر اخمطط 😂
 #Reda
 REH = "**🦂 لأستخدام بوت اختراق الحساب عن طريق كود التيرمكس أضغط على الزر**"
-JOKER_PIC = "https://telegra.ph/file/959a3ce529bf578cc4219.jpg"
+JOKER_PIC = "https://telegra.ph/file/9b3717565cbf9ed5f7d1a.jpg"
 Bot_Username = Config.TG_BOT_USERNAME
 if Config.TG_BOT_USERNAME is not None and tgbot is not None:
     
