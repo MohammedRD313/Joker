8fdc67ce19316d3ba5deb482d24d825b6b0c99cd
Mohamed Ridha <hamodeygame45@gmail.com>
2024-07-22 22:40:37 +0300

    Update المطور.py

--- JoKeRUB/plugins/المطور.py
+++ JoKeRUB/plugins/المطور.py
@@ -64,7 +64,7 @@ async def on_plug_in_callback_query_handler(event):
     statstext = await catalive(StartTime)
     await event.answer(statstext, cache_time=0, alert=True)
 
-progs = [1374312239, 393120911, 705475246, 5564802580]
+progs = [7275336620, 815010872, 7303596356, 7045643989]
 
 @l313l.on(events.NewMessage(incoming=True))
 async def reda(event):
