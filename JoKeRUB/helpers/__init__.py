3d30d430ba493fc4d0796dc8ea46fc03ace7b6b0
Mohamed Ridha <hamodeygame45@gmail.com>
2024-08-02 16:02:34 +0300

    Update __init__.py

--- JoKeRUB/helpers/__init__.py
+++ JoKeRUB/helpers/__init__.py
@@ -2,7 +2,7 @@ from . import fonts
 from . import memeshelper as catmemes
 from .aiohttp_helper import AioHttp
 from .utils import *
-import post_to_telegraph
+from .some_module import post_to_telegraph
 
 def post_to_telegraph(title, content):
 
