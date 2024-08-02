773c8cec8a2729a083eafdcf4f7381ebf08bc6be
Mohamed Ridha <hamodeygame45@gmail.com>
2024-08-02 12:20:57 +0300

    Update chatbot.py

--- JoKeRUB/helpers/chatbot.py
+++ JoKeRUB/helpers/chatbot.py
@@ -1,16 +1,11 @@
-import asyncio
-from randomstuff import AsyncClient
-from .Config import Config
+from .utils.extdl import install_pip
 
-loop = asyncio.get_event_loop()
-rs_client = None
+try:
+    import randomstuff
+except ModuleNotFoundError:
+    install_pip("randomstuff.py")
+    import randomstuff
 
-async def initialize_client():
-    global rs_client
-    rs_client = AsyncClient(api_key=Config.RANDOM_STUFF_API_KEY, version="4")
+from ..Config import Config
 
-# تأكد من استدعاء `initialize_client` في السياق المناسب
-if not loop.is_running():
-    loop.run_until_complete(initialize_client())
-else:
-    asyncio.ensure_future(initialize_client())
+rs_client = randomstuff.AsyncClient(api_key=Config.RANDOM_STUFF_API_KEY, version="4")
