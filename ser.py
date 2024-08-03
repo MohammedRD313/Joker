1afd89243c79baff281329e471ed1cab08553cf7
Mohamed Ridha <hamodeygame45@gmail.com>
2024-08-03 15:16:25 +0300

    Update ser.py

--- ser.py
+++ ser.py
@@ -29,4 +29,4 @@ def internal_error(error):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=True)  # قم بإيقاف التشغيل debug في الإنتاج
+    app.run(host="0.0.0.0", port=port)  # قم بإيقاف التشغيل debug في الإنتاج
