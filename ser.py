20eaaa9f9588471125b96616ea458c9c1d6fd4bf
Mohamed Ridha <hamodeygame45@gmail.com>
2024-08-03 16:52:12 +0300

    Update ser.py

--- ser.py
+++ ser.py
@@ -1,16 +1,5 @@
-import os
-from flask import Flask
-from flask_restful import Resource, Api
-
-app = Flask(__name__)
-api = Api(app)
-
-class Greeting(Resource):
-    def get(self):
-        return {"message": "𝗦𝗰𝗼𝗿𝗽𝗶𝗼 𝘄𝗼𝗿𝗸𝘀 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 ✅"}
-
-api.add_resource(Greeting, '/')
+from waitress import serve
+from app import app  # تأكد من تعديل هذا حسب مسار تطبيقك
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    serve(app, host='0.0.0.0', port=8000)
