04c576f9d41cede7db0ded007806f17b4e1891d6
Mohamed Ridha <hamodeygame45@gmail.com>
2024-08-05 01:29:05 +0300

    Update media_utils.py

--- JoKeRUB/helpers/media_utils.py
+++ JoKeRUB/helpers/media_utils.py
@@ -1,9 +1,14 @@
 def media_type(content_type):
-    if content_type.startswith('application/json'):
-        return 'JSON'
-    elif content_type.startswith('text/html'):
-        return 'HTML'
-    elif content_type.startswith('text/plain'):
-        return 'Plain Text'
+    # تأكد من أن content_type هو نص
+    if isinstance(content_type, str):
+        if content_type.startswith('application/json'):
+            return 'JSON'
+        elif content_type.startswith('text/html'):
+            return 'HTML'
+        elif content_type.startswith('text/plain'):
+            return 'Plain Text'
+        else:
+            return 'Unknown'
     else:
+        # إذا لم يكن content_type نصًا، ارجع 'Unknown'
         return 'Unknown'
